3bdf7577bbea54dfce33ed8ac5c4f591eb253f14
luciano.ramello <luciano.ramello@vass.es>
2021-01-20 12:35:45 +0100

    add new repo files

---
+++ http/config.py
@@ -0,0 +1,14 @@
+# Device name
+device_id = 'DEVICE_ID'
+
+# Select sensors to use 1 = use | 0 = skip
+enviromental = 1
+gyroscope = 1
+accelerometer = 1
+magnetometer = 1
+
+# Select relay interval
+relay = 30
+
+# Define endpoint
+endpoint = 'http://127.0.0.1:8080/sensor_data'
